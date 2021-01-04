f10baef1a1dddfdaff07e68ab3cd4ed06a87d0e2
Stephanie <stephaniejmars@gmail.com>
2021-01-03 20:41:11 -0800

    heap sort complete)

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,23 @@
-
+# require_relative "min_heap"
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
+# Time Complexity:  O(nlogn)
+# Space Complexity: O(nlogn)
 def heapsort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+
+  return list if list.length - 1 <= 1
+
+  heap = MinHeap.new
+
+  list.each do |value|
+    heap.add(value)
+  end
+
+  result = []
+  
+  list.length.times do
+    result << heap.remove.value
+  end
+
+  return result
 end
