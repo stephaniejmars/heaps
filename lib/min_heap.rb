145983af74f130d81cbfb5231dbed4d90fecdbaa
Stephanie <stephaniejmars@gmail.com>
2021-01-03 20:42:02 -0800

    min heap incomplete

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -14,10 +14,11 @@ class MinHeap
   end
 
   # This method adds a HeapNode instance to the heap
-  # Time Complexity: ?
-  # Space Complexity: ?
+  # Time Complexity: O(logn)
+  # Space Complexity: O(1)
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    @store << HeapNode.new(key, value)
+    heap_up(@store.length - 1)
   end
 
   # This method removes and returns an element from the heap
@@ -25,7 +26,14 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    return nil if @store.empty?
+
+    swap(0, @store.length - 1)
+    result = @store.pop
+    heap_down(0)
+    return result
+
+
   end
 
 
@@ -47,7 +55,7 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return if @store.empty? 
   end
 
   private
@@ -58,14 +66,38 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def heap_up(index)
-    
+    return if index == 0 
+    parent_index = (index - 1) / 2
+    if @store[parent_index].key > @store[index].key
+      swap(index, parent_index)
+      heap_up(parent_index)
+    end
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    return if index >= @store.length - 1
+    
+    left_index = index * 2 + 1
+    right_index = index * 2 + 2
+
+    return if !@store[left_index] && !@store[right_index]
+
+    smallest = left_index
+
+    if left_index
+      if (right_index < @store.length) && (@store[right_index].key < @store[left_index].key)
+        smallest = right_index
+      end
+
+      if @store[smallest].key < @store[index].key
+        swap(index, smallest)
+      end
+      heap_down(smallest)
+    end
+
   end
 
   # If you want a swap method... you're welcome
